adf779dc930407b65f2484859922fcb367eea9d0
elieceruiz <69103366+elieceruiz@users.noreply.github.com>
2026-03-09 22:18:28 -0500

    Implement YNAB API functions for categories and transactions

---
+++ ynab_api.py
@@ -0,0 +1,45 @@
+import requests
+import streamlit as st
+from datetime import date
+
+TOKEN = st.secrets["YNAB_TOKEN"]
+BUDGET = st.secrets["BUDGET_ID"]
+
+def traer_categorias():
+
+    url = f"https://api.ynab.com/v1/budgets/{BUDGET}/categories"
+
+    headers = {"Authorization": f"Bearer {TOKEN}"}
+
+    r = requests.get(url, headers=headers).json()
+
+    categorias = []
+
+    for group in r["data"]["category_groups"]:
+        for cat in group["categories"]:
+            if not cat["deleted"]:
+                categorias.append({
+                    "nombre": cat["name"],
+                    "id": cat["id"]
+                })
+
+    return categorias
+
+
+def crear_transaccion(account_id, categoria_id, payee, amount):
+
+    url = f"https://api.ynab.com/v1/budgets/{BUDGET}/transactions"
+
+    payload = {
+        "transaction": {
+            "account_id": account_id,
+            "date": str(date.today()),
+            "amount": -int(amount * 1000),
+            "payee_name": payee,
+            "category_id": categoria_id
+        }
+    }
+
+    headers = {"Authorization": f"Bearer {TOKEN}"}
+
+    requests.post(url, json=payload, headers=headers)
